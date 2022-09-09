38aa1d9b68cf0a3b8d94673277844affa5c1db83
Aigul <sharipova.aig@gmail.com>
2022-09-09 15:19:14 -0400

    let

--- waves/waves.js
+++ waves/waves.js
@@ -1,6 +1,8 @@
-const timer = 1;
-const amountTime = timer * 60;
+const timer = 5;
+let amountTime = timer * 60;
 
 function calculateTime() {
   const countdown = document.querySelector('#countdown');
+  let minutes = Math.floor(amountTime/60);
+  let seconds = amountTime % 60;
 }
