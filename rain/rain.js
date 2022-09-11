4de1cee00749f1781f6a571f1e873744e8bff7c0
Aigul <sharipova.aig@gmail.com>
2022-09-11 14:23:20 -0400

    if rain

--- rain/rain.js
+++ rain/rain.js
@@ -3,8 +3,15 @@ const amountTime = timer * 60;
 
 function calculateTime() {
   const countdown = document.querySelector('#countdown');
+
   let minutes = Math.floor(amountTime/60);
   let seconds = amountTime % 60;
+
+  if(amountTime < 0) {
+    stopTimer()
+    amountTime = 0
+  }
+  
   countdown.textContent = `${minutes} : ${seconds}`;
   amountTime --;
 }
